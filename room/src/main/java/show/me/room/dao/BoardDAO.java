fe5c4480b2b8ec10225cdab138476c5ad1e4a8f7
YONGMIN-JEONG <skyline706@naver.com>
2019-02-25 20:59:10 +0900

    2월 25일 야간

--- room/src/main/java/show/me/room/dao/BoardDAO.java
+++ room/src/main/java/show/me/room/dao/BoardDAO.java
@@ -1,5 +1,15 @@
 package show.me.room.dao;
 
-public class BoardDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+//BoardDAO(Invite, Pr, QnA)
+@Repository
+public class BoardDAO {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	
 }
